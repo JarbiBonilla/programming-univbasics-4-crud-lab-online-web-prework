1ee72310b19a85c47f9cf37a92c330d105e33706
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-09-27 05:06:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  famous_dogs << ""
-p the_office
+  famous_dogs << "Cheddar"
+p famous_dogs
 end
 
 def add_element_to_start_of_array(array, element)
