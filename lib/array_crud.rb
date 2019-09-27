cd838eecaa57fff6ee4310dd3feb0fc579328fb2
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-09-27 04:50:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,9 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  the_office = ["Michael", "Dwight"]
   the_office << "Pam"
-  p the_office  
 end
 
 def add_element_to_start_of_array(array, element)
